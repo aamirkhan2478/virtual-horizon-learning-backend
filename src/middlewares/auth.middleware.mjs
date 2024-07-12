ef190cf2a9cdb92754bc52db434bcbc1214fe8d0
aamirkhan2478 <aamir.khan2478@gmail.com>
2024-07-12 09:45:17 +0500

    chore: Update environment variables and add auth middleware

---
+++ src/middlewares/auth.middleware.mjs
@@ -0,0 +1,41 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/user.model.js");
+
+const verifyToken = (token) => {
+  if (!token) {
+    throw new Error("No token provided", 401);
+  }
+
+  try {
+    return jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+  } catch (error) {
+    throw new Error("Invalid token", 401);
+  }
+};
+
+const findUser = async (id) => {
+  const user = await User.query().findById(id).select("id", "name", "email", "userType");
+
+  if (!user) {
+    throw new Error("Invalid access token", 401);
+  }
+
+  return user;
+};
+
+const auth = async (req, res, next) => {
+  try {
+    const bearerToken = req.headers.authorization;
+    const token = bearerToken.split(" ")[1];
+    const decoded = verifyToken(token);
+    req.user = await findUser(decoded.id);
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      message: error?.message || "Not authorized to access this route",
+      success: false,
+    });
+  }
+};
+
+module.exports = auth;
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,30 +3,28 @@ const express = require("express");
 
 const {
     register,
-    // login,
-    // verifyEmail,
-    // forgotPassword,
-    // getUser,
-    // resetPassword,
+    login,
+    verifyEmail,
+    forgotPassword,
+    resetPassword,
     // updateUser,
-    // changePassword,
+    changePassword,
     // updateImage,
-    // resendEmail,
+    resendEmail,
     // getAllUsers,
 } = require("../controllers/user.controllers.js");
 
 const router = express.Router();
 
 router.post("/register", register);
-// router.post("/verify-email", verifyEmail);
-// router.post("/login", login);
-// router.post("/forgot-password", forgotPassword);
-// router.post("/reset-password", resetPassword);
-// router.get("/current-user", auth, getUser);
+router.post("/verify-email", verifyEmail);
+router.post("/login", login);
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password", resetPassword);
 // router.put("/update-user", auth, updateUser);
-// router.put("/change-password", auth, changePassword);
+router.put("/change-password", auth, changePassword);
 // router.patch("/update-image", auth, updateImage);
-// router.post("/resend-email", resendEmail);
+router.post("/resend-email", resendEmail);
 // router.get("/all", auth, getAllUsers);
 
 module.exports = router;
