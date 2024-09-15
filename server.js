0c2f3fbf5ede3a2fabc77628cc287ab143ae3d91
aamirkhan2478 <aamir.khan2478@gmail.com>
2024-09-15 16:56:51 +0500

    Refactor server.js to include notification routes

--- server.js
+++ server.js
@@ -1,14 +1,28 @@
+// Initiate the server and connect to the database
 require("dotenv").config();
+
+// import express for building the server
 const express = require("express");
+
+// import http for creating the server
 const http = require("http");
+
+// import cors for allowing requests from the client
 const cors = require("cors");
+
+// import routes for routing
 const userRouter = require("./src/routes/user.routes.js");
 const resourceRouter = require("./src/routes/resource.routes.js");
+const notificationRouter = require("./src/routes/notification.routes.js");
+
+// import auth middleware for authentication
 const auth = require("./src/middlewares/auth.middleware.js");
 const {
   errorHandler,
   notFound,
 } = require("./src/middlewares/error.middleware.js");
+
+// import database for connecting to the database
 const db = require("./src/database/db.js");
 
 // Initialize database
@@ -46,6 +60,7 @@ app.get("/", (_req, res) => {
 // Routes
 app.use("/api/user", userRouter);
 app.use("/api/resource", auth, resourceRouter);
+app.use("/api/notification", auth, notificationRouter);
 
 app.use(notFound);
 app.use(errorHandler);
